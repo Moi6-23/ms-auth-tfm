090634f3005916e070f35916d3f740c21c26e399
Moi6-23 <ariasmoises993@gmail.com>
2025-05-14 18:27:31 -0500

    Initial commit

---
+++ src/main/java/tfm/unir/ing/ms_auth_tfm/MsAuthTfmApplication.java
@@ -0,0 +1,13 @@
+package tfm.unir.ing.ms_auth_tfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthTfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthTfmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfm/unir/ing/ms_auth_tfm/MsAuthTfmApplicationTests.java
@@ -0,0 +1,13 @@
+package tfm.unir.ing.ms_auth_tfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthTfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
